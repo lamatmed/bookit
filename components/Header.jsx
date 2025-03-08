f7ca961dca91036feea35cbda502c89b10552387
lamatmed <lamat032025@gmail.com>
2025-03-09 00:00:15 +0100

    first commit traduit

--- components/Header.jsx
+++ components/Header.jsx
@@ -89,15 +89,15 @@ const Header = () => {
         <div className='bg-gray-200 md:hidden'>
           <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
             <Link href='/' className='block px-3 py-2 text-base font-medium text-gray-800 rounded-md hover:bg-gray-700 hover:text-white'>
-              Rooms
+            Salles
             </Link>
             {isAuthenticated && (
               <>
                 <Link href='/bookings' className='block px-3 py-2 text-base font-medium text-gray-800 rounded-md hover:bg-gray-700 hover:text-white'>
-                  Bookings
+                Réservations
                 </Link>
                 <Link href='/rooms/add' className='block px-3 py-2 text-base font-medium text-gray-800 rounded-md hover:bg-gray-700 hover:text-white'>
-                  Add Room
+                Ajouter Salle
                 </Link>
               </>
             )}
