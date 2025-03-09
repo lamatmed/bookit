5dabd562119fcae9ddee0a014e7549107f588f36
lamatmed <lamat032025@gmail.com>
2025-03-09 17:16:14 +0100

    first commit traduit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="fixed bottom-0 w-full py-4 bg-gray-100">
+    <footer className="w-full p-4 py-4 mt-10 bg-green-100 ">
       <div className="flex flex-col items-center px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <p className="text-sm text-center">
           &copy; {currentYear} <span className="font-semibold">Bookit</span>. Tous droits réservés.
